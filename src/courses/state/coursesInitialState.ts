ca064e4aafe9a920cbb29651a811631e4b95ba0e
Nicholas Morrow <njmorrow95@gmail.com>
2020-06-21 03:14:08 -0400

    course page routing

--- src/courses/state/coursesInitialState.ts
+++ src/courses/state/coursesInitialState.ts
@@ -2,11 +2,22 @@ import { CoursesState } from '~/courses/types/CoursesState';
 
 export const coursesInitialState: CoursesState = {
     courses: [
-        { courseId: 1, name: 'Math' },
         {
-            courseId: 2,
-            name: 'History',
+            courseId: 1,
+            name: 'Math',
+            learningObjectives: [
+                {
+                    learningObjectiveId: 1,
+                    title: 'Divison',
+                    description: 'Students should be able to...',
+                    projects: [
+                        {
+                            projectId: 1,
+                            description: 'Project 1',
+                        },
+                    ],
+                },
+            ],
         },
-        { courseId: 3, name: 'English' },
     ],
 };
--- src/courses/types/Course.ts
+++ src/courses/types/Course.ts
@@ -1,4 +1,38 @@
 export interface Course {
     courseId: number;
     name: string;
+    learningObjectives: LearningObjective[];
+}
+
+interface LearningObjective {
+    learningObjectiveId: number;
+    title: string;
+    description: string;
+    projects: Project[];
+}
+
+interface Project {
+    projectId: number;
+    description: string;
+}
+
+interface Student {
+    studentId: number;
+    name: string;
+}
+
+interface CourseFeedback {
+    feedbackId: number;
+    content: string;
+    student: Student;
+    course: Course;
+    date: Date;
+}
+
+interface ProjectFeedback {
+    projectFeedbackId: number;
+    content: string;
+    student: Student;
+    date: Date;
+    project: Project;
 }
